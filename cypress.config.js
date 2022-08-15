d58aa9b20c5c22c61482a654fb835e06c6c6bf82
Jesse Lind <Jesseclind@gmail.com>
2022-08-15 13:29:12 -0400

    test: add cypress project id

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    projectId: '4sdnhr',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
